f7585ee93e6b3735522bc8ffb1eb9083f30b4d1d
haryava <rakotohf@icloud.com>
2023-03-13 21:22:38 +0100

    fast

--- sources/calc.py
+++ sources/calc.py
@@ -1,5 +1,5 @@
 """
-La librairie calc permet de faire les opérations basiques de calcul entre deux entiers.
+La librairie calc permet de faire les opérations basiques de calcul entre deux entiers(test gpf).
 """
 
 def add(arg1,arg2):
